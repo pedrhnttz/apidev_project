3f1031bef2d7b17a78fd84e883134162bd6d1435
Lucas Eduardo <lucas.eduardo0112@outlook.com>
2025-04-25 21:29:52 -0300

    Atualização class Professor

--- professor/professores_model.py
+++ professor/professores_model.py
@@ -1,15 +1,39 @@
-dados = {
-    'professores': [
-        {
-            'id': 0,
-            'nome': 'Caio',
-            'materia': 'Desenvolvimento de APIs E Microsserviços',
-            'obs': 'Especialista em Microsserviços'
-        }
-    ]
-}
+from flask import jsonify
+from datetime import datetime,date
+from config import db
 
-IdProfessor = 0
+class Professor(db.Model):
+    __tablename__ = "professores"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome = db.Column(db.String(100), nullable = False)
+    data_nascimento = db.Column(db.Date, nullable = False)
+    idade = db.Column(db.Integer, nullable = False)
+    disciplina = db.Column(db.String(50), nullable = False)
+    salario = db.Column(db.Float, nullable = False)
+
+    def __init__(self, nome, data_nascimento, idade, disciplina, salario):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.idade = idade
+        self.disciplina = disciplina
+        self.salario = salario
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "nome":self.nome,
+            "data_nascimento":self.data_nascimento,
+            "idade":self.idade,
+            "disciplina":self.disciplina,
+            "salario":self.salario
+        }
+    
+    def calcularIdade(self):
+        today = date.today()
+        return today.year - self.data_nascimento.year - ((today.month, today.day) < (self.data_nascimento.month, self.data_nascimento.day))
+
+
 
 # Classes de exceções
 
@@ -24,71 +48,52 @@ class ProfessorInvalid(Exception):
 # Funções de rota
 
 def get_professores():
-    return dados['professores']
+    professores = Professor.query.all()
+    return [professor.to_dict() for professor in professores]
 
 def get_professor_by_id(id_professor):
-    professores = dados['professores']
-    for professor in professores:
-        if professor.get('id') == id_professor:
-            return professor
-    raise ProfessorNotFound
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNotFound
+    return professor.to_dict()
+
 
 def create_professor(professor):
+    if not professor or 'nome' not in professor:
+        return jsonify({'erro': 'professor sem nome'}), 400
+    data_nascimento_str = professor.get('data_nascimento')
+    data_nascimento = datetime.strptime(data_nascimento_str, '%Y-%m-%d').date()
 
-    professor['nome'] = create_nome(professor)
-    professor['materia'] = create_materia(professor.get('materia'))
-    professor['obs'] = create_obs(professor.get('obs'))
-    professor['id'] = create_id(professor.get('id', None))
+    novo_professor = Professor(
+        nome = professor['nome'],
+        disciplina = professor['disciplina'],
+        data_nascimento = data_nascimento,
+        salario = professor['salario']
+    )
+    novo_professor.idade = novo_professor.calcularIdade()
+    db.session.add(novo_professor)
+    db.session.commit()
 
-    dados['professores'].append(professor)
-    return {'msg': 'Professor criado!'}
+    return jsonify(professor), 200
 
 def update_professor(id_professor, professor_up):
-    professor = get_professor_by_id(id_professor)
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        return jsonify({'erro':'professor nao encontrado'})
+    professor.nome = professor_up['nome']
+    professor.disciplina = professor_up['disciplina']
+    professor.data_nascimento = professor_up['data_nascimento']
+    professor.salario = professor_up['salario']
+    professor.idade = professor.calcularIdade()
 
-    professor_up['nome'] = create_nome(professor_up)
-    professor_up['materia'] = create_materia(professor_up.get('materia'))
-    professor_up['obs'] = create_obs(professor_up.get('obs'))
-
-    professor.update(professor_up)
     return {'msg': 'Professor atualizado!'}
 
 def delete_professor(professor_id):
-    professores = dados['professores']
-    professor = get_professor_by_id(professor_id)
+    professor = Professor.query.get(professor_id)
+    if not professor:
+        raise Exception('Não foi possível deletar o professor')
+    db.session.delete(professor)
+    db.session.commit()
+    return jsonify({'mensagem': 'Professor deletado'}), 200
 
-    professores.remove(professor)
-
-    if professor not in professores:
-        return {'msg': 'Professor deletado!'}
-    raise Exception('Não foi possível deletar o professor')
     
-# Funções de lógica
-
-def create_nome(professor):
-    if not professor or 'nome' not in professor:
-        raise ProfessorInvalid
-    nome = professor.get('nome')
-    return nome
-
-def create_materia(materia):
-    if not materia:
-        raise Exception('Matéria inválida')
-    return materia
-
-def create_obs(obs):
-    if not obs:
-        raise Exception('Observação inválida')
-    return obs
-
-def create_id(id_professor):
-    global IdProfessor
-    professores = dados['professores']
-    if id_professor:
-        for professor in professores:
-            if professor['id'] == id_professor:
-                raise Exception('Este ID já está sendo utilizado')
-        return id_professor
-    else:
-        IdProfessor += 1
-        return IdProfessor
